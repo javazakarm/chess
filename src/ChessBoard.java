e8d7e448420dab198f9cf7f704adef2f10c93528
javazakarm <58331661+javazakarm@users.noreply.github.com>
2021-05-02 18:53:32 +0400

    Add files via upload

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,53 +1,53 @@
 public class ChessBoard {
-    String[][] board = new String[8][8];
+    private String[][] board = new String[9][9];
     private int sumOfColumnAndRow;
     private int differenceOfColumnAndRow;
-//    public void board(int figures, int column, int row){
-//      switch (figures){
-//            case 1:
-//                pawn(row, column);
-//                break;
-//            case 2:
-//                rook(row, column);
-//                break;
-//            case 3:
-//                knight(row, column);
-//                break;
-//            case 4:
-//                bishop(row, column);
-//                break;
-//            case 5:
-//                queen(column, row);
-//                break;
-//            case 6:
-//                king(row, column);
-//                break;
-//        }
-//    }
-    public void pawn(int column,int row){
-        board[row][column] = "P";
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (i == row && j == column){
-                    System.out.print(board[row][column] + "   ");
-                }else if ((i == row - 1) && (j == column - 1 || j == column + 1)) {
-                    board[i][j] = "A";
-                    System.out.print(board[i][j] + "   ");
-                }
-                else {
-                    System.out.print("." + "   ");
-                }
-            }
-            System.out.println();
+
+    public void board(int figures, int column, int row) {
+        switch (figures) {
+            case 1:
+                pawn(column, row);
+                break;
+            case 2:
+                rook(column, row);
+                break;
+            case 3:
+                knight(column, row);
+                break;
+            case 4:
+                bishop(column, row);
+                break;
+            case 5:
+                queen(column, row);
+                break;
+            case 6:
+                king(column, row);
+                break;
+
         }
     }
-    public void rook(int column,int row){
-        board[row][column] = "R";
+
+    public void pawn(int column, int row) {
+        char name = 'P';
+        board[8][0] = "  ";
+        char t = 'a';
+        int k = 8;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (i == row && j == column){
+
+                if (i == 8 && j == 0) {
+                    System.out.print(board[8][0] + "  ");
+                } else if (i == 8) {
+                    board[row][column] = String.valueOf(t);
+                    t++;
                     System.out.print(board[row][column] + "   ");
-                }else if (i == row || j == column) {
+                } else if (j == 0) {
+                    board[row][column] = String.valueOf(k);
+                    k--;
+                    System.out.print(board[row][column] + "   ");
+                } else if (i == row && j == column) {
+                    System.out.print(name + "   ");
+                } else if ((i == row - 1) && (j == column - 1 || j == column + 1)) {
                     board[i][j] = "A";
                     System.out.print(board[i][j] + "   ");
                 } else {
@@ -57,35 +57,92 @@ public class ChessBoard {
             System.out.println();
         }
     }
-    public void knight(int column,int row){
-        board[row][column] = "H";
+
+    public void rook(int column, int row) {
+        char name = 'R';
+        board[8][0] = "  ";
+        char t = 'a';
+        int k = 8;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (i == row && j == column){
+                if (i == 8 && j == 0) {
+                    System.out.print(board[8][0] + "  ");
+                } else if (i == 8) {
+                    board[row][column] = String.valueOf(t);
+                    t++;
                     System.out.print(board[row][column] + "   ");
-                }else if ((i == row - 2 || i == row + 2) && (j ==column - 1 || j == column + 1)) {
+                } else if (j == 0) {
+                    board[row][column] = String.valueOf(k);
+                    k--;
+                    System.out.print(board[row][column] + "   ");
+                } else if (i == row && j == column) {
+                    System.out.print(name + "   ");
+                } else if (i == row || j == column) {
                     board[i][j] = "A";
                     System.out.print(board[i][j] + "   ");
-                }else if ((i ==row - 1 || i == row + 1 ) && (j == column - 2 ||  j == column + 2)){
-                    board[i][j] = "A";
-                    System.out.print(board[i][j] + "   ");
-                }
-                else {
+                } else {
                     System.out.print("." + "   ");
                 }
             }
             System.out.println();
         }
     }
-    public void bishop(int column,int row){
+
+    public void knight(int column, int row) {
+        char name = 'H';
+        board[8][0] = "  ";
+        char t = 'a';
+        int k = 8;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (i == 8 && j == 0) {
+                    System.out.print(board[8][0] + "  ");
+                } else if (i == 8) {
+                    board[row][column] = String.valueOf(t);
+                    t++;
+                    System.out.print(board[row][column] + "   ");
+                } else if (j == 0) {
+                    board[row][column] = String.valueOf(k);
+                    k--;
+                    System.out.print(board[row][column] + "   ");
+                } else if (i == row && j == column) {
+                    System.out.print(name + "   ");
+                } else if ((i == row - 2 || i == row + 2) && (j == column - 1 || j == column + 1)) {
+                    board[i][j] = "A";
+                    System.out.print(board[i][j] + "   ");
+                } else if ((i == row - 1 || i == row + 1) && (j == column - 2 || j == column + 2)) {
+                    board[i][j] = "A";
+                    System.out.print(board[i][j] + "   ");
+                } else {
+                    System.out.print("." + "   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void bishop(int column, int row) {
         sumOfColumnAndRow = row + column;
         differenceOfColumnAndRow = column - row;
-        board[row][column] = "B";
+        char name = 'B';
+        board[8][0] = "  ";
+        char t = 'a';
+        int k = 8;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (i == row && j == column) {
+                if (i == 8 && j == 0) {
+                    System.out.print(board[8][0] + "  ");
+                } else if (i == 8) {
+                    board[row][column] = String.valueOf(t);
+                    t++;
                     System.out.print(board[row][column] + "   ");
-                }else if (i + j == sumOfColumnAndRow || ((j - i) == differenceOfColumnAndRow)) {
+                } else if (j == 0) {
+                    board[row][column] = String.valueOf(k);
+                    k--;
+                    System.out.print(board[row][column] + "   ");
+                } else if (i == row && j == column) {
+                    System.out.print(name + "   ");
+                } else if (i + j == sumOfColumnAndRow || ((j - i) == differenceOfColumnAndRow)) {
                     board[i][j] = "A";
                     System.out.print(board[i][j] + "   ");
                 } else {
@@ -95,15 +152,29 @@ public class ChessBoard {
             System.out.println();
         }
     }
+
     public void queen(int column, int row) {
         sumOfColumnAndRow = row + column;
         differenceOfColumnAndRow = column - row;
-        board[row][column] = "Q";
+        char name = 'Q';
+        board[8][0] = "  ";
+        char t = 'a';
+        int k = 8;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (i == row && j == column)
+                if (i == 8 && j == 0) {
+                    System.out.print(board[8][0] + "  ");
+                } else if (i == 8) {
+                    board[row][column] = String.valueOf(t);
+                    t++;
                     System.out.print(board[row][column] + "   ");
-                else if (i + j == sumOfColumnAndRow || ((j - i) == differenceOfColumnAndRow) || (i == row) || (j == column)) {
+                } else if (j == 0) {
+                    board[row][column] = String.valueOf(k);
+                    k--;
+                    System.out.print(board[row][column] + "   ");
+                } else if (i == row && j == column) {
+                    System.out.print(/*board[row][column]*/ name + "   ");
+                } else if (i + j == sumOfColumnAndRow || ((j - i) == differenceOfColumnAndRow) || (i == row) || (j == column)) {
                     board[i][j] = "A";
                     System.out.print(board[i][j] + "   ");
                 } else {
@@ -113,17 +184,30 @@ public class ChessBoard {
             System.out.println();
         }
     }
-    public void king(int column,int row){
-        board[row][column] = "K";
+
+    public void king(int column, int row) {
+        char name = 'K';
+        board[8][0] = "  ";
+        char t = 'a';
+        int k = 8;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (i == row && j == column) {
+                if (i == 8 && j == 0) {
+                    System.out.print(board[8][0] + "  ");
+                } else if (i == 8) {
+                    board[row][column] = String.valueOf(t);
+                    t++;
                     System.out.print(board[row][column] + "   ");
-                }
-                else if ((i > row - 2 && i < row + 2) && (j > column - 2 && j < column + 2)) {
+                } else if (j == 0) {
+                    board[row][column] = String.valueOf(k);
+                    k--;
+                    System.out.print(board[row][column] + "   ");
+                } else if (i == row && j == column) {
+                    System.out.print(name + "   ");
+                } else if ((i > row - 2 && i < row + 2) && (j > column - 2 && j < column + 2)) {
                     board[i][j] = "A";
                     System.out.print(board[i][j] + "   ");
-                } else{
+                } else {
                     System.out.print("." + "   ");
                 }
             }
--- src/ChessService.java
+++ src/ChessService.java
@@ -1,61 +1,62 @@
-public class ChessService{
-           public int rowCase(int row){
-            switch (row){
-                case 1:
-                    row = 7;
-                    break;
-                case 2:
-                    row = 6;
-                    break;
-                case 3:
-                    row = 5;
-                    break;
-                case 4:
-                    row = 4;
-                    break;
-                case 5:
-                    row = 3;
-                    break;
-                case 6:
-                    row = 2;
-                    break;
-                case 7:
-                    row = 1;
-                    break;
-                case 8:
-                    row = 0;
-                    break;
-            }
-            return row;
+public class ChessService {
+    public int rowCase(int row) {
+        switch (row) {
+            case 1:
+                row = 7;
+                break;
+            case 2:
+                row = 6;
+                break;
+            case 3:
+                row = 5;
+                break;
+            case 4:
+                row = 4;
+                break;
+            case 5:
+                row = 3;
+                break;
+            case 6:
+                row = 2;
+                break;
+            case 7:
+                row = 1;
+                break;
+            case 8:
+                row = 0;
+                break;
         }
-        public int columnCase(String column){
-            int columnInt = 0;
-            switch (column){
-                case "a":
-                    columnInt = 0;
-                    break;
-                case "b":
-                    columnInt = 1;
-                    break;
-                case "c":
-                    columnInt = 2;
-                    break;
-                case "d":
-                    columnInt = 3;
-                    break;
-                case "e":
-                    columnInt = 4;
-                    break;
-                case "f":
-                    columnInt = 5;
-                    break;
-                case "g":
-                    columnInt = 6;
-                    break;
-                case "h":
-                    columnInt = 7;
-                    break;
-            }
-            return columnInt;
+        return row;
+    }
+
+    public int columnCase(char column) {
+        int columnInt = 0;
+        switch (column) {
+            case 'a':
+                columnInt = 1;
+                break;
+            case 'b':
+                columnInt = 2;
+                break;
+            case 'c':
+                columnInt = 3;
+                break;
+            case 'd':
+                columnInt = 4;
+                break;
+            case 'e':
+                columnInt = 5;
+                break;
+            case 'f':
+                columnInt = 6;
+                break;
+            case 'g':
+                columnInt = 7;
+                break;
+            case 'h':
+                columnInt = 8;
+                break;
         }
+        return columnInt;
+    }
 }
